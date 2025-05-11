f389ba969bb0615c3c3d008309055e9528f7c405
Maksym.Zyza <93919982+maksym-zyza-telus@users.noreply.github.com>
2025-05-11 17:49:12 +0300

    fix: refactor code, add logging

--- book_library.py
+++ book_library.py
@@ -1,62 +1,80 @@
 from abc import ABC, abstractmethod
+from typing import List
+import logging
 
-# SRP — Book class stores only book-related data
+# Cтворюємо логер, рівень INFO
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+logger = logging.getLogger(__name__)
+
+
+# Виконується принцип єдиної відповідальності (SRP), клас Book відповідає за зберігання інформації про книгу
 class Book:
-    def __init__(self, title: str, author: str, year: str):
+    def __init__(self, title: str, author: str, year: str) -> None:
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
 
-# ISP — LibraryInterface defines specific methods required for working with a library
+
+# Виконується принцип відкритості/закритості (OCP), клас Library міг бути розширений для нової функціональності без зміни коду.
 class LibraryInterface(ABC):
     @abstractmethod
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         pass
 
     @abstractmethod
-    def get_books(self) -> list[Book]:
+    def get_books(self) -> List[Book]:
         pass
 
-# OCP, LSP — Library implements the interface and can be substituted without breaking functionality
+
+# Принцип підстанови Лісков (LSP), будь-який клас, який наслідує інтерфейс LibraryInterface, може замінити клас Library без порушення роботи програми.
+# Використовується інтерфейс LibraryInterface для чіткої специфікації методів, які необхідні для роботи з бібліотекою library
 class Library(LibraryInterface):
-    def __init__(self):
-        self._books: list[Book] = []
+    def __init__(self) -> None:
+        self.books: List[Book] = []
 
-    def add_book(self, book: Book):
-        self._books.append(book)
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
 
-    def remove_book(self, title: str):
-        self._books = [book for book in self._books if book.title != title]
+    def remove_book(self, title: str) -> None:
+        self.books[:] = [book for book in self.books if book.title != title]
 
-    def get_books(self) -> list[Book]:
-        return self._books
+    def get_books(self) -> List[Book]:
+        return self.books
 
-# DIP — LibraryManager depends on abstraction (LibraryInterface) rather than a concrete implementation
+
+# Виконується принцип інверсії залежностей (DIP), класи вищого рівня, такі як LibraryManager, залежали від абстракцій (інтерфейсів), а не від конкретних реалізацій класів
 class LibraryManager:
-    def __init__(self, library: LibraryInterface):
+    def __init__(self, library: LibraryInterface) -> None:
         self.library = library
 
-    def add_book(self, title: str, author: str, year: str):
+    def add_book(self, title: str, author: str, year: str) -> None:
         book = Book(title, author, year)
         self.library.add_book(book)
+        logger.info(f"Added book: {book}")
 
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         self.library.remove_book(title)
+        logger.info(f"Removed book: {title}")
 
-    def show_books(self):
+    def show_books(self) -> None:
         books = self.library.get_books()
         if not books:
-            print("No books in the library.")
+            logger.info("No books in the library.")
         else:
             for book in books:
-                print(book)
+                logger.info(str(book))
+
 
 def main():
     library = Library()
@@ -79,7 +97,8 @@ def main():
             case "exit":
                 break
             case _:
-                print("Invalid command. Please try again.")
+                logger.warning("Invalid command. Please try again.")
+
 
 if __name__ == "__main__":
     main()
--- factory_pattern.py
+++ factory_pattern.py
@@ -1,70 +1,91 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+import logging
+
+# Cтворюємо логер, рівень INFO
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
+
+# Створюємо файловий handler для логера, рівень ERROR:
+filename = Path(__file__).stem
+fh = logging.FileHandler(f"{filename}.log")
+fh.setLevel(logging.ERROR)
+fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(fh)
 
 
-# Abstract base class for all vehicles
+# Створюємо абстрактний базовий клас Vehicle
 class Vehicle(ABC):
-    def __init__(self, make, model, region_spec="") -> None:
+    def __init__(self, make: str, model: str, region_spec="") -> None:
         self.make = make
         self.model = model
         self.region_spec = region_spec
-    
+
     @abstractmethod
     def start_engine(self):
         pass
 
 
-# Car and Motorcycle classes implementing start_engine
+# Car та Motorcycle успадковуються від Vehicle
 class Car(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} {self.region_spec}: Двигун запущено")
+    def start_engine(self) -> None:
+        logger.info(f"{self.make} {self.model} {self.region_spec}: Двигун запущено")
 
 
 class Motorcycle(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} {self.region_spec}: Мотор заведено")
+    def start_engine(self) -> None:
+        logger.info(f"{self.make} {self.model} {self.region_spec}: Мотор заведено")
 
 
-# Abstract factory for creating vehicles
+# Створюємо абстрактний клас VehicleFactory
 class VehicleFactory(ABC):
     @abstractmethod
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         pass
 
     @abstractmethod
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         pass
 
 
-# Factory for creating vehicles with US specifications
+# Реалізуємо USVehicleFactory та EUVehicleFactory. Cтворюють автомобілі та мотоцикли з позначкою регіону
 class USVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "US Spec")
-    
-    def create_motorcycle(self, make, model):
+
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "US Spec")
 
 
-# Factory for creating vehicles with EU specifications
 class EUVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str):
         return Car(make, model, "EU Spec")
-    
-    def create_motorcycle(self, make, model):
+
+    def create_motorcycle(self, make: str, model: str):
         return Motorcycle(make, model, "EU Spec")
 
 
-# Create objects using vehicle factories
-us_factory = USVehicleFactory()
-eu_factory = EUVehicleFactory()
+def main():
+    # Використовуються фабрики для створення транспортних засобів
+    us_factory = USVehicleFactory()
+    eu_factory = EUVehicleFactory()
 
-vehicle1 = us_factory.create_car("Ford", "Mustang")
-vehicle2 = us_factory.create_motorcycle("Harley-Davidson", "Sportster")
+    vehicle1 = us_factory.create_car("Ford", "Mustang")
+    vehicle2 = us_factory.create_motorcycle("Harley-Davidson", "Sportster")
 
-vehicle3 = eu_factory.create_car("Renault", "Duster")
-vehicle4 = eu_factory.create_motorcycle("Yamaha", "R1")
+    vehicle3 = eu_factory.create_car("Renault", "Duster")
+    vehicle4 = eu_factory.create_motorcycle("Yamaha", "R1")
 
-# Start engines
-vehicle1.start_engine()
-vehicle2.start_engine()
-vehicle3.start_engine()
-vehicle4.start_engine()
+    vehicle1.start_engine()
+    vehicle2.start_engine()
+    vehicle3.start_engine()
+    vehicle4.start_engine()
+
+
+if __name__ == "__main__":
+    main()
